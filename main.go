fe04dd2294c354f6442a110ed946aa68eae64bb5
Philip Krause <pkrause@ip-10-75-0-2.ec2.internal>
2020-01-30 16:29:38 -0500

    firstcommit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	shoot()
+
+}
+
+func shoot() {
+	rand.Seed(time.Now().UnixNano())
+
+	ai := randomInt(0, 3)
+	input := 0
+
+	fmt.Println("Choose 0 for Rock, 1 for Paper, 2 for Scissors.")
+	fmt.Scanln(&input)
+
+	switch {
+
+	case input == ai:
+		fmt.Println("DRAW")
+
+	case input == 0 && ai == 1:
+		fmt.Println("You Lose. Paper beats Rock")
+
+	case input == 0 && ai == 2:
+		fmt.Println("You Win. Rock beats Scissors")
+
+	case input == 1 && ai == 2:
+		fmt.Println("You Lose. Scissors beats Paper")
+
+	case input == 1 && ai == 0:
+		fmt.Println("You Win. Paper beats Rock")
+
+	case input == 2 && ai == 0:
+		fmt.Println("You Lose. Rock beats Scissors")
+
+	case input == 2 && ai == 1:
+		fmt.Println("You Win. Scissors beats Rock")
+	}
+
+}
+
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
